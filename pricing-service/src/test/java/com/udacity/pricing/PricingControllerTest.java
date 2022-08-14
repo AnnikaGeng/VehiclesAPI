fcf777c60702a86b82970180305340ea8694d966
AnnikaGeng <annika.geng@foxmail.com>
2022-08-14 12:38:39 +0800

    version 1.0

---
+++ pricing-service/src/test/java/com/udacity/pricing/PricingControllerTest.java
@@ -0,0 +1,41 @@
+package com.udacity.pricing;
+
+import com.udacity.pricing.api.PricingController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import java.net.URI;
+
+import static org.mockito.Mockito.times;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(PricingController.class)
+public class PricingControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    PricingController pricingController;
+
+    @Test
+    public void getPrice() throws Exception {
+        mockMvc.perform(get("/services/price?vehicleId=1"))
+                .andExpect(status().isOk());
+
+    }
+}
---
+++ pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -0,0 +1,18 @@
+package com.udacity.pricing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PricingServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+}
